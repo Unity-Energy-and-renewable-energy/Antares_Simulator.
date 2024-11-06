0c9eca85011d30b7404ab4d79e1be72dddc8c15c
CAMUS Benjamin <benjamin.camus@scalian.com>
2024-11-06 16:53:28 +0100

    fix bug occuring when not all the thermal clusters are participating to a reserve

--- src/solver/optimisation/constraints/POutCapacityThreasholds.cpp
+++ src/solver/optimisation/constraints/POutCapacityThreasholds.cpp
@@ -107,7 +107,7 @@ void POutCapacityThreasholds::add(int pays, int cluster, int pdt)
             data.areaReserves[pays].areaCapacityReservationsDown);
 
         builder.data.NbTermesContraintesPourLesReserves
-            += (nbTermsUp + 1) * (nbTermsUp > 0) + (nbTermsDown + 1) * (nbTermsDown > 0);
+            += (nbTermsUp + 2) * (nbTermsUp > 0) + (nbTermsDown + 2) * (nbTermsDown > 0);
 
         builder.data.nombreDeContraintes += (nbTermsUp > 0) + (nbTermsDown > 0);
     }
